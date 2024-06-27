65b22772b3fc10f69679c6fb1f84f3419e08288c
Soumyajit (Sam) <soumyajit.basu22@gmail.com>
2024-06-27 12:13:07 +0530

    Improving Pipeline & Text Extractor/PDF Reader to Return List

--- analyze_docs.py
+++ analyze_docs.py
@@ -2,12 +2,14 @@ import argparse
 import dotenv
 import os
 from utils.analysis.analyzer import analyze_doc
+import json
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Analyze the documentation of a project.')
     parser.add_argument('--doc_path', type=str, help='The path to the project to analyze.')
     parser.add_argument('--prompt_path', type=str, help='The path to the prompt file (txt).')
     parser.add_argument('--augment_links', type=bool)
+    parser.add_argument('--country', type=str, help='The country of the project.')
     return parser.parse_args()
 
 
@@ -20,11 +22,23 @@ def main():
 
     # Load the prompt
     with open(args.prompt_path, 'r') as file:
-        prompt = file.read()
+        prompt = json.load(file)
 
     # Analyze the documentation
-    doc_analysis = analyze_doc(args.doc_path, question=prompt, augment_link=args.augment_links)
+    doc_analysis = {}
+    for i, question in enumerate(prompt["questions"].keys()):
+        previous_context = None
+        query = prompt["questions"][question] + '\n' + prompt["postfix"][question]
+        print(query)
+        if i > 0:
+            # Previous context is the previous answers (j<i) concatenated
+            previous_context = '\n\n'.join([str(doc_analysis[f"Q{j+1}"]) for j in range(i)])
+            # previous_context = doc_analysis[f"Q{i}"]
+        doc_analysis[f"Q{i+1}"] = analyze_doc(args.doc_path, question=query, augment_link=args.augment_links, previous_context=previous_context, model_name='gpt-3.5-turbo')
     print(doc_analysis)
+    # Save the analysis in Json file
+    with open(f'doc_analysis_{args.country}.json', 'w') as file:
+        json.dump(doc_analysis, file, indent=4)
 
 if __name__ == "__main__":
     main()
--- utils/analysis/analyzer.py
+++ utils/analysis/analyzer.py
@@ -1,25 +1,31 @@
 from PyPDF2 import PdfReader
 import os
+import shutil
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_core.runnables import RunnablePassthrough
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
+from langchain_core.prompts import PromptTemplate
 from langchain import hub
 import requests
 
     
-def read_pdf(file_path: str):
+def read_pdf(file_path: str, return_list: bool = False):
     pdf = PdfReader(file_path)
-    text = ''
+    text = []
     for page in pdf.pages:
-        text += page.extract_text()
-        text += '\n'
-    return text
+        text = page.extract_text()
+        text.append(text)
+
+    if return_list:
+        return text
+    else:
+        return '\n\n'.join(text)
 
 
 def extract_hyperlinks(pdf_reader):
-    links = ()
+    links = set()
     for page in pdf_reader.pages:
         if '/Annots' in page:
             for annot in page['/Annots']:
@@ -44,14 +50,19 @@ def augment_link_content(file_path: str):
     for link in links:
         if link.endswith(".pdf"):
             response = requests.get(link)
+            if response.status_code != 200:
+                continue
 
-            os.makedirs("file_cache")
+            os.makedirs("file_cache", exist_ok=True)
             with open("file_cache/cache.pdf", "wb") as file:
                 file.write(response.content)
 
-            augmentation_text += read_pdf("file_cache/cache.pdf") + '\n'
-            os.removedirs("file_cache")
-            
+            try:
+                augmentation_text += read_pdf("file_cache/cache.pdf") + '\n'
+            except:
+                pass
+            shutil.rmtree("file_cache")
+
     return augmentation_text
 
 def split_and_store_db(text: str):
@@ -68,7 +79,7 @@ def get_llm(model_name: str = 'gpt-3.5-turbo'):
     return ChatOpenAI(model_name=model_name)
 
 
-def analyze_doc(file_path: str, question: str, augment_link: str = False, model_name: str = 'gpt-3.5-turbo'):
+def analyze_doc(file_path: str, question: str, augment_link: str = False, previous_context: str = None, model_name: str = 'gpt-3.5-turbo'):
     text = read_pdf(file_path)
     if augment_link:
         print("Augmenting Text With Content From Hyperlinks")
@@ -83,17 +94,29 @@ def analyze_doc(file_path: str, question: str, augment_link: str = False, model_
     llm = get_llm(model_name=model_name)
 
     # Define prompt
-    prompt = hub.pull("rlm/rag-prompt")
+    template = '''Use the following pieces of context to answer the question at the end in JSON format.
+        If you don't know the answer, just retun None as value, don't try to make up an answer.
+        
+
+        Context: {context}
+
+        Question: {question}
+
+        Answer: 
+    '''
+    prompt = PromptTemplate.from_template(template)
 
     # Rag Chain
     rag_chain = (
         {"context": retriever | join_context, "question": RunnablePassthrough()}
         | prompt
         | llm
-        | StrOutputParser()
+        | JsonOutputParser()
     )
 
     # Run the chain
+    if previous_context:
+        question = previous_context + '\n\n' + question
     answer = rag_chain.invoke(question)
 
     return answer
