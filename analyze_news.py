672a114060542bc3bb9fbf96fd47da911fb00240
Soumyajit (Sam) <soumyajit.basu22@gmail.com>
2024-11-08 16:26:02 +0530

    Added News Analysis Code

---
+++ analyze_news.py
@@ -0,0 +1,14 @@
+from utils.analysis.analyzer import analyze_doc
+import json
+
+def analyze_news(news: str, questions: json, model_name: str = 'gpt-3.5-turbo'):
+    prompt = questions["prompt"]
+    questions = questions["questions"]
+
+    question = f"{prompt}\n\n" + "\n\n".join(questions)
+    # print(question)
+    return analyze_doc(news, question, model_name=model_name)
+    
+    
+    
+        
